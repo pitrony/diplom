a5acba5bad7009de4f30aef2e89f367d4ad64e93
pitrony <158518294+pitrony@users.noreply.github.com>
2024-12-17 19:00:49 +0000

    Update build.bat
    correct w lib

--- orange_test_stend_c/build.bat
+++ orange_test_stend_c/build.bat
@@ -1,7 +1,7 @@
 gcc -g -c --std=c11 -lconio -Wall -lwiringPi -o test_board.o test_board.c
 gcc -g -c --std=c11 -lconio -Wall -o decoder_data.o decoder_data.c
 gcc -g -c --std=c11 -lconio -Wall -o pub_mqtt.o pub_mqtt.c
-gcc -o test_board.exe test_board.o decoder_data.o pub_mqtt.o
+gcc -o test_board.exe test_board.o decoder_data.o pub_mqtt.o -L/usr/local/lib -lwiringPi -lpaho-mqtt3c
 pause
 test_board.exe
 pause
