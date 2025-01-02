fce176937d851acef089cbc0a92da42544708ee3
pitrony <158518294+pitrony@users.noreply.github.com>
2025-01-02 16:37:19 -0500

    Update pub_mqtt.c
    correct main

--- orange_test_stend_c/pub_mqtt.c
+++ orange_test_stend_c/pub_mqtt.c
@@ -1,32 +1,16 @@
 #include "test_board.h"
+#include <string.h>
 #include <MQTTClientPersistence.h>
-//#include <MQTTClient.h>
-// #include "D:\alexey\iot_lerning\vc_projects\orang_test_elevator_board\paho.mqtt.c-master\src\MQTTClient.h"
 #include <MQTTAsync.h>
-#include "paho.mqtt.c-master/src/samples/pubsub_opts.h"
 
-
-
-#if defined(_WIN32)
-#define sleep Sleep
-#else
-#include <sys/time.h>
-#endif
-
-
-// #define ADDRESS     "tcp://localhost:1883"
+//#define ADDRESS     "tcp://localhost:1883"
 #define ADDRESS     "mqtt.eclipseprojects.io:1883"
-//#define ADDRESS     "192.168.1.85:1883"
 #define CLIENTID    "Orangepi"
-//#define TOPIC "/orangepi/data1"
-//~ #define PAYLOAD     "Hello World!"
-// #define PAYLOAD     "32"
+
 #define QOS         1
 #define TIMEOUT     10000L
-int data=33;
-char* TOPIC = "/orangepi/data1";
-int send_mqtt(char* TOPIC, int data)
-{
+
+int send_mqtt(char* TOPIC, int _data){
 //int  PAYLOAD=_data;
 MQTTClient client;
     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
@@ -45,17 +29,18 @@ MQTTClient client;
         printf("Failed to connect, return code %d\n", rc);
         exit(-1);
     }
+    printf("connect, return code %d\n", rc);
     //Encoding.UTF8.GetBytes(message)
     //pubmsg.payload = atoi((char *)_data);
-    pubmsg.payload = &data;
+    pubmsg.payload = &_data;
     //pubmsg.payloadlen =64;
-    pubmsg.payloadlen = sizeof(data);
+    pubmsg.payloadlen = sizeof(_data);
     pubmsg.qos = QOS;
     pubmsg.retained = 0;
     MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
     printf("Waiting for up to %d seconds for publication of %d\n"
             "on topic %s for client with ClientID: %s\n",
-            (int)(TIMEOUT/1000), data, TOPIC, CLIENTID);
+            (int)(TIMEOUT/1000), _data, TOPIC, CLIENTID);
     rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
     printf("Message with delivery token %d delivered\n", token);
     MQTTClient_disconnect(client, 10000);
@@ -63,12 +48,3 @@ MQTTClient client;
     return rc;
 
 }
-
-int main()
-{
-	
-	int ret=send_mqtt(TOPIC, data);
-	
-	return ret;
-	
-	}
